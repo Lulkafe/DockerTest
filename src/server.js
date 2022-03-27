56b6b4979d52787a6c85ac26835d444fe1b5490e
Seimei Matsusaki <seimatsusaki@gmail.com>
2022-03-27 08:16:14 -0400

    Removed unnecessary files/lines

--- src/server.js
+++ src/server.js
@@ -4,12 +4,9 @@ const path = require('path')
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
-
 
 app.listen(port);
 console.log('Started listening on port ' + port);
